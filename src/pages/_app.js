7bf77712068f915ef748ba5eabe330e036f00fd5
Kushal Bogati <kushalbogati@Kushals-MacBook-Air.local>
2023-03-26 20:18:56 +0545

    adding metas

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Middlman - Outsourcing made easy</title>
+        <meta name="description" content="Streamline your business with Middlman's outsourcing arbitrage." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Script src="https://apps.elfsight.com/p/platform.js" defer />
       <Layout>
