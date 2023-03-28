89c137741fff23002da64c9e64e33d966d2bb22e
Kushal Bogati <kushalbogati@Kushals-MacBook-Air.local>
2023-03-28 09:27:46 +0545

    improving code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
 import Header from '@/components/header';
-import VideoSection from '@/components/video-section';
 import ListSection from '@/components/list-section';
 import FeatureSection from '@/components/feature-section';
-import CasesSection from '@/components/cases-section';
-import SocialProof from '@/components/social-proof';
 import PricingTable from '@/components/pricing-table';
 import Footer from '@/components/footer';
-import Review from '@/components/review';
 
 import { WhatsAppWidget } from 'react-whatsapp-widget';
 import 'react-whatsapp-widget/dist/index.css';
